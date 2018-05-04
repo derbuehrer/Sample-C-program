d26bfa8c262bf7a7a3bf0489f1076d4f0f192af1
derbuehrer <derbuehrer@gmx.de>
2018-05-04 08:26:41 +0200

    Sample C program for tutorials

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ int main() {
     var1 = 1337;
     int var2 = var0 + var1;
 
+    int c = getchar();      // wartet auf eine 1byte große angabe des benutzers (auch buchstaben, da diese auch zahlen sind...)
+    test(c);
+
     const float var3 = 2.5;     // const ist eine konstante variable, final variable
     double var4 = 2.64119846;
     printf("addiert: %d \n",var2);      // %d steht stellvertrehtend für var2
@@ -18,7 +21,15 @@ int main() {
     printf("%f\n", PI);
 
     // if, else if und else sind wie sonst immer
-
+    // switch case anweisungen sind wie gewohnt
     // alle operatoren sind wie gewohnt
+    // alle schleifen sind gleich
+
+
     return 0;
+}
+
+int test(int c) {
+    printf("dies ist eine weitere funktion\n");
+    printf("die taste die du gedrückt hast, hat den keycode %d\n", c);
 }
